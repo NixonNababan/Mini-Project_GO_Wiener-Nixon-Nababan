5f181db4ac6a0873a7cd05aee975fe445b52b123
NixonNababan <105839395+NixonNababan@users.noreply.github.com>
2023-05-12 20:03:44 +0800

    perbaikan collaborator, biar user bisa join sekali saja

--- usecase/collabolator.go
+++ usecase/collabolator.go
@@ -1,8 +1,22 @@
 package usecase
 
-import "mini-project/lib/database"
+import (
+	"mini-project/lib/database"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 func CreateCollabolator(taskId uint, userId uint) error {
+	task, err := database.GetTask(taskId)
+	if err != nil {
+		return err
+	}
+	for _, value := range task.Collaborators {
+		if value.UserID == userId {
+			return echo.NewHTTPError(http.StatusBadRequest, "you have join the task")
+		}
+	}
 	if err := database.CreateCollabolator(taskId, userId); err != nil {
 		return err
 	}
